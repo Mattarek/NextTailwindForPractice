9f97cd6d23a55ab756c937f31c4679cbf0dc46e4
Marcin Barc <marb637@gmail.com>
2024-11-11 22:34:23 +0100

    Files with basic routes and styles

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+        <main className="px-5 pt-32 text-center">
+            <h1 className="mb-5 text-4xl font-bold md:text-5xl">
+                Welcome to my blog
+            </h1>
+            <p className="mx-auto max-w-[750px] leading-8">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+                dicta maxime facere ut ab. Et laboriosam consequatur nostrum
+                blanditiis harum at unde molestiae dolores? Consectetur qui
+                accusamus expedita harum dolorum.
+            </p>
+        </main>
+    );
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Posts() {
+    return (
+        <main className="px-5 pt-32 text-center">
+            <h1 className="mb-5 text-4xl font-bold md:text-5xl">All posts</h1>
+        </main>
+    );
+}
